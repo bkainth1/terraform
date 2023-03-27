312a68ff9c51af8b6bfc36b0805ad62b720ce49a
bkainth1 <99654679+bkainth1@users.noreply.github.com>
2023-03-27 09:24:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAQB5OPGX3YTCIKSR7"
-  secret_key = "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"
-
-}
-
 
 resource "aws_instance" "myec2" {
     ami = "ami-0c75f58fc78dd920e"
