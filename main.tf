7e5966e763bb9cffb00cf1c3a14bd0a75cff3071
bkainth1 <99654679+bkainth1@users.noreply.github.com>
2025-08-11 16:45:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,59 @@
-provider "aws" {
-  region     = "us-west-2"
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-0c75f58fc78dd920e"
-    instance_type = "t2.micro"
-  
+resource "azurerm_resource_group" "demo_rg" {
+  name     = "demo-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "demo_vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.demo_rg.location
+  resource_group_name = azurerm_resource_group.demo_rg.name
+}
+
+resource "azurerm_subnet" "demo_subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = azurerm_resource_group.demo_rg.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "demo_nic" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.demo_rg.location
+  resource_group_name = azurerm_resource_group.demo_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.demo_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "demo_vm" {
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.demo_rg.name
+  location            = azurerm_resource_group.demo_rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.demo_nic.id,
+  ]
+  admin_password = "P@ssword1234!" # For testing only. Use SSH keys in production.
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
 
